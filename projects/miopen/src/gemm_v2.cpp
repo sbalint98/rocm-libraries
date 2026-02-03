d7b7e1f765a92a45ac40f80e2f1920c122e992b6
Bálint Soproni <balint@streamhpc.com>
2026-02-03 14:21:39 +0000

    recent rocblas does not require k%4 for int8

--- projects/miopen/src/gemm_v2.cpp
+++ projects/miopen/src/gemm_v2.cpp
@@ -1295,7 +1295,7 @@ miopenStatus_t CallGemmStridedBatchedSequential(const Handle& handle,
         switch(gemm_desc.dataType)
         {
         case miopenInt8: {
-            assert(gemm_desc.k % 4 == 0);
+            // assert(gemm_desc.k % 4 == 0);
 
             auto alpha = int(gemm_desc.alpha);
             auto beta  = int(gemm_desc.beta);
